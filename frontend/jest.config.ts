9ab869fd011922905e79d99423cbd26a33c4d2e5
Brent Mills <brent.c.mills@gmail.com>
2026-03-10 19:13:30 -0700

    Rewrite navigation sidebar with workflow-oriented hierarchy

---
+++ frontend/jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  modulePathIgnorePatterns: ["<rootDir>/.next/"],
+};
+
+export default createJestConfig(config);
