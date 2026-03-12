f393da033a16a1d00c068f7fe6a73aabd7ecb3d1
Brent Mills <brent.c.mills@gmail.com>
2026-03-12 11:27:04 -0700

    Add Pub/Sub resources to Terraform storage module

--- backend/terraform/modules/storage/outputs.tf
+++ backend/terraform/modules/storage/outputs.tf
@@ -22,3 +22,13 @@ output "config_backups_bucket_name" {
   description = "Name of the config backups GCS bucket"
   value       = google_storage_bucket.config_backups.name
 }
+
+output "pubsub_topic_name" {
+  description = "Name of the Pub/Sub topic for ingest bucket notifications"
+  value       = google_pubsub_topic.ingest_events.name
+}
+
+output "pubsub_subscription_name" {
+  description = "Name of the Pub/Sub subscription for the ingest worker"
+  value       = google_pubsub_subscription.ingest_worker.name
+}
