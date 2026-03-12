7b371b896b31977ccbbd833a91d7548d8e6a5258
Brent Mills <brent.c.mills@gmail.com>
2026-03-11 22:37:18 -0700

    Add Terraform compute module with GKE cluster, pipeline and interactive node pools

---
+++ terraform/modules/compute/variables.tf
@@ -0,0 +1,49 @@
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP region"
+}
+
+variable "zone" {
+  type        = string
+  description = "GCP zone for the zonal GKE cluster"
+}
+
+variable "org_slug" {
+  type        = string
+  description = "Organization slug used in resource naming"
+}
+
+variable "k8s_pipeline_machine_type" {
+  type        = string
+  default     = "n2-highmem-8"
+  description = "Machine type for the pipeline node pool"
+}
+
+variable "k8s_pipeline_max_nodes" {
+  type        = number
+  default     = 20
+  description = "Maximum number of nodes in the pipeline autoscaler"
+}
+
+variable "k8s_pipeline_use_spot" {
+  type        = bool
+  default     = true
+  description = "Whether the pipeline pool uses spot instances"
+}
+
+variable "k8s_interactive_machine_type" {
+  type        = string
+  default     = "n2-standard-4"
+  description = "Machine type for the interactive node pool"
+}
+
+variable "k8s_interactive_max_nodes" {
+  type        = number
+  default     = 5
+  description = "Maximum number of nodes in the interactive autoscaler"
+}
